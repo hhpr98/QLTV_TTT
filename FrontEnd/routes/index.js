2765f7573f4c3134ec3d6f668072f6a53f3c95f3
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2019-12-30 05:36:28 +0700

    chỉnh sửa layout + thêm view + chỉnh sửa url + param

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{redirectPage:'',namePage:'Trang chủ'});
+});
+
+router.get('/book-management', function(req, res, next) {
+  res.render('book-management',{redirectPage:'book-management',namePage:'Quản lí sách'});
 });
 
 module.exports = router;
