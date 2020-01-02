9b04087e6490cd9176659db26f3d39986c104bb0
anhoapham2302 <hoaan31071998@gmail.com>
2020-01-02 22:33:38 +0700

    update book-management, book-add by AnHoa

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -9,7 +9,9 @@ router.get('/', function(req, res, next) {
 router.get('/book-management', function(req, res, next) {
   res.render('book-management',{redirectPage:'book-management',namePage:'Quản lí sách'});
 });
-
+router.get('/book-add', function(req, res, next) {
+  res.render('book-add',{redirectPage:'book-add',namePage:'Thêm sách'});
+});
 router.get('/profile', function (req, res, next) {
   res.render('profile',{redirectPage:'profile',namePage:'Thông tin tài khoản'});
 });
