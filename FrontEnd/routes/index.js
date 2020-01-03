b2f0f424e6ef4da6b6b506cfdcae5864e31d01f8
unknown <you@example.com>
2020-01-03 22:42:47 +0700

    Quan li tra muon

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -15,7 +15,9 @@ router.get('/book-add', function(req, res, next) {
 router.get('/profile', function (req, res, next) {
   res.render('profile',{redirectPage:'profile',namePage:'Thông tin tài khoản'});
 });
-
+router.get('/quanlitramuon', function (req, res, next) {
+  res.render('QuanLiTraMuon',{redirectPage:'QuanLiTraMuon',namePage:'Thông tin tài khoản'});
+});
 router.get('/maps', function (req, res, next) {
   res.render('maps',{redirectPage:'maps',namePage:'Bản đồ'});
 });
