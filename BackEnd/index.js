8e2a5292df44fb2a4e9f0a7807b22687248b1af3
khauthanhhieu <thanhhieu021298@gmail.com>
2020-01-03 23:46:35 +0700

    init backend và thêm .gitignore cho fontend

---
+++ BackEnd/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser())
+
+app.get('/', function (req, res) {
+  res.send('Xin chào, đây là API')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Web app service listening on port ${PORT}!`)
+})
