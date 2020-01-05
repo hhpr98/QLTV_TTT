dc1b26e248c497f30a109de5069aab15f3e05990
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2020-01-06 06:53:29 +0700

    getJSON fail

--- FrontEnd/routes/index.js
+++ FrontEnd/routes/index.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
+//const http = require('http');
+//const https = require('https');
+//var jsonnnn = require('../controller/getJSON.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index',{redirectPage:'',namePage:'Trang chủ'});
 });
 
+//var dat =
+
 router.get('/book-management', function(req, res, next) {
+  /*
+  jsonnnn.getJSON({
+    host: 'localhost:4200',
+    path: '/api/book',
+    method: 'GET'
+  }).then(({status, data}) => {
+    res.json(data);
+  }, (error) => {
+    next(error);
+  });
+   */
+
   res.render('book-management',{redirectPage:'book-management',namePage:'Quản lí sách'});
 });
+
+
+
 router.get('/book-add', function(req, res, next) {
   res.render('book-add',{redirectPage:'book-add',namePage:'Thêm sách'});
 });
@@ -16,10 +36,12 @@ router.get('/profile', function (req, res, next) {
   res.render('profile',{redirectPage:'profile',namePage:'Thông tin tài khoản'});
 });
 router.get('/quanlitramuon', function (req, res, next) {
-  res.render('QuanLiTraMuon',{redirectPage:'QuanLiTraMuon',namePage:'Thông tin tài khoản'});
+  res.render('QuanLiTraMuon',{redirectPage:'quanlitramuon',namePage:'Quản lí trả mượn'});
 });
 router.get('/maps', function (req, res, next) {
   res.render('maps',{redirectPage:'maps',namePage:'Bản đồ'});
 });
 
 module.exports = router;
+
+
